f2ac88f30f80c9d3d28115ba26d505a150b57199
aamina-siddiqui <aaminas211@gmail.com>
2026-04-23 19:43:00 +0000

    updated S3 bucket ACL to separate resource

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "aamin-terraform-lab-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
+
