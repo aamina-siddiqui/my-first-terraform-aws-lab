9bbd12684b678dc05bcafd0002c5d779aa2b213a
aamina-siddiqui <aaminas211@gmail.com>
2026-04-23 21:01:45 +0000

    updated bucket config without ACL

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "example" {
   bucket = "aamin-terraform-lab-bucket"
-}
 
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
+  tags = {
+    Environment = "Dev"
+    Owner       = "Aamina"
+  }
 }
 
